0d01a613166e16ce048e2695452f67b45cc4dd88
Oivlisfriend <robertomarcelogongo@gmail.com>
2023-11-26 16:26:40 +0100

    NLW COPA

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,17 +2,34 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: 'Roboto, sans-serif'
       },
+      backgroundImage: {
+        app: 'url(/app-bg.png)'
+      },
+      colors: {
+        ignite: {
+          500: '#129E57'
+        },
+        gray: {
+          900: "#121214",
+          600: "#323238",
+          800: "#202024",
+          300: '#8D8D99',
+          100: "#E1E1E6"
+        },
+        yellow: {
+          700: '#E5CD3D',
+          500: '#F7DD43'
+        }
+      }
     },
   },
   plugins: [],
